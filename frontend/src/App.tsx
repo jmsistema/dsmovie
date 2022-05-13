42be8215393a74b46504f2a0e3bcd6436c282e54
jmsistema <michelsantana004@gmail.com>
2022-05-13 13:13:55 -0300

    NavBar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,18 @@
 function App() {
   return (
-    <h1>Página Principal</h1>
+<header>
+    <nav className="container">
+        <div className="dsmovie-nav-content">
+            <h1>DSMovie</h1>
+            <a href="https://github.com/devsuperior" target="_blank" rel="noreferrer">
+                <div className="dsmovie-contact-container">
+                    
+                    <p className="dsmovie-contact-link">/devsuperior</p>
+                </div>
+            </a>
+        </div>
+    </nav>
+</header>
   );
 }
 
